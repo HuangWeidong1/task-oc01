412e814751b30cf9fbb2e6755df4ec387744c2d0
中软mini025 <zhongruanmini025@zhongruanmini025deMac-mini.local>
2015-08-15 08:59:39 +0800

    Initial Commit

---
+++ task-oc01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  task-oc01
+//
+//  Created by 中软mini025 on 15/8/15.
+//  Copyright (c) 2015年 中软mini025. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
